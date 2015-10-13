9a6dcf992c21fc4c656c484cd28da92cdfd150a0
mindwing <mindwing@gmail.com>
2015-10-13 13:53:09 +0900

    hide actionbar

--- app/src/main/java/kr/mindwing/webbrowser/MainActivity.java
+++ app/src/main/java/kr/mindwing/webbrowser/MainActivity.java
@@ -1,7 +1,9 @@
 package kr.mindwing.webbrowser;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        hideActionBar();
+    }
+
+    private void hideActionBar() {
+        View decorView = getWindow().getDecorView();
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
     }
 }
