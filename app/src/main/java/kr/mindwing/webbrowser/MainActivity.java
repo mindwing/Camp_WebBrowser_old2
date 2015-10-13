fd20894629880439f9fbad50c51995d32618fb7b
mindwing <mindwing@gmail.com>
2015-10-13 17:58:50 +0900

    nav buttons

--- app/src/main/java/kr/mindwing/webbrowser/MainActivity.java
+++ app/src/main/java/kr/mindwing/webbrowser/MainActivity.java
@@ -8,12 +8,15 @@ import android.view.View;
 import android.view.View.OnKeyListener;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText textUrl;
     private WebView webView;
+    private Button backButton, forwardButton;
     private OnKeyListener listener = new OnKeyListener() {
 
         @Override
@@ -52,12 +55,35 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setupUI() {
+        backButton = (Button) findViewById(R.id.back);
+        backButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (webView.canGoBack()) {
+                    webView.goBack();
+                } else {
+                    Toast.makeText(MainActivity.this, "맨 뒷페이지 입니다.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        forwardButton = (Button) findViewById(R.id.forward);
+
         textUrl = (EditText) findViewById(R.id.text_url);
         textUrl.setOnKeyListener(listener);
-        textUrl.setText("http://facebook.com");
+        textUrl.setText("http://google.com");
 
         webView = (WebView) findViewById(R.id.webview);
         webView.setWebViewClient(new WebViewClient());
+    }
+
+    public void goForward(View view) {
+        if (webView.canGoForward()) {
+            webView.goForward();
+        } else {
+            Toast.makeText(MainActivity.this, "맨 앞페이지 입니다.", Toast.LENGTH_SHORT)
+                    .show();
+        }
     }
 
     private void hideActionBar() {
